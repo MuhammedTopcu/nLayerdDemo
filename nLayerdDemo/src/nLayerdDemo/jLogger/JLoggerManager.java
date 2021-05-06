bc6f7e7f731604d339165ea14ea8ec8598b68e48
Muhammed TOPCU <Muhammed.topcu@outlook.com>
2021-05-06 23:09:10 +0300

    ek

--- nLayerdDemo/src/nLayerdDemo/jLogger/JLoggerManager.java
+++ nLayerdDemo/src/nLayerdDemo/jLogger/JLoggerManager.java
@@ -2,6 +2,6 @@ package nLayerdDemo.jLogger;
 
 public class JLoggerManager {
 	public void log(String message) {
-		System.out.println("J Logger ile loglandý");
+		System.out.println("J Logger ile loglandý " + message);
 	}
 }
